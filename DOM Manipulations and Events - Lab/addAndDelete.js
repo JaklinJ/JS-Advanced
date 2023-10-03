4ea0419ad2727fa09e396e2b72c6544169a8be27
Jaklin Jeleva <122488493+JaklinJ@users.noreply.github.com>
2023-10-03 14:02:47 +0300

    Update addAndDelete.js

--- DOM Manipulations and Events - Lab/addAndDelete.js
+++ DOM Manipulations and Events - Lab/addAndDelete.js
@@ -1,1 +1,22 @@
-addItem
+function addItem() {
+    let inputField = document.getElementById("newItemText").value;
+    let parent = document.getElementById("items");
+
+    if (inputField === 0) return;
+    let newLi = document.createElement("li");
+    newLi.textContent = inputField;
+    let remove = document.createElement('a');
+    let linkText = document.createTextNode("[Delete]");
+    remove.appendChild(linkText)
+    remove.href = "#";
+    remove.addEventListener('click', deleteLink);
+
+    newLi.appendChild(remove);
+    parent.appendChild(newLi);
+
+
+    function deleteLink() {
+       newLi.remove();
+
+    }
+}
