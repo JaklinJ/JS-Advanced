6cddfa4f460e1619ced1cbf011c6f57310d4a1b3
Jaklin Jeleva <122488493+JaklinJ@users.noreply.github.com>
2023-10-14 22:13:40 +0300

    Create list.js

---
+++ Classes - Exercise/list.js
@@ -0,0 +1,29 @@
+class List {
+    constructor(list = []) {
+        this.list = list.sort((a, b) => a - b);
+        this.size = this.list.length;
+    }
+
+    add(element) {
+       this.list.push(element);
+       this.list.sort((a, b) => a - b);
+       this.size++;
+       return
+    }
+    remove(index) {
+        if(index < 0 || index >= this.list.length) {
+            throw new Error('Error: Invalid index!')
+        } else {
+            this.list.splice(index,1);
+            this.size--;
+            return;
+        }
+    }
+    get(index) {
+        if(index < 0 || index >= this.list.length) {
+            throw new Error('Error: Invalid index!')
+        } else {
+            return this.list[index];
+        }
+    }
+}
